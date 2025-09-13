55264d63e91f28d10e5294ba3ccca84c310fde72
Tizian Everke <t.everke@gmail.com>
2025-09-13 12:33:30 +0200

    feat: Implement basic Quick Settings tile service

---
+++ app/src/main/java/com/curryplayer/quicksettingssoundprofile/services/SoundProfileService.kt
@@ -0,0 +1,75 @@
+package com.curryplayer.quicksettingssoundprofile.services
+
+import android.media.AudioManager
+import android.service.quicksettings.TileService
+import android.util.Log
+
+class SoundProfileService: TileService() {
+
+    // Called when the user adds your tile.
+    override fun onTileAdded() {
+        super.onTileAdded()
+        Log.i("SoundProfileService", "Tile added")
+        updateSoundTile()
+    }
+
+    // Called when your app can update your tile.
+    override fun onStartListening() {
+        super.onStartListening()
+        Log.i("SoundProfileService", "Tile start listening")
+
+    }
+
+    // Called when your app can no longer update your tile.
+    override fun onStopListening() {
+        super.onStopListening()
+        Log.i("SoundProfileService", "Tile stop listening")
+    }
+
+    // Called when the user taps on your tile in an active or inactive state.
+    override fun onClick() {
+        super.onClick()
+        Log.i("SoundProfileService", "Tile clicked")
+        val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+        val currentMode = audioManager.ringerMode
+        Log.i("Current Sound Mode", currentMode.toString())
+        //updateSoundTile()
+    }
+
+    // Called when the user removes your tile.
+    override fun onTileRemoved() {
+        super.onTileRemoved()
+        Log.i("SoundProfileService", "Tile removed")
+    }
+
+    private fun updateSoundTile() {
+        val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+        val currentMode = audioManager.ringerMode
+        Log.i("Current Sound Mode", currentMode.toString())
+
+        val isQsTileNull = (qsTile == null)
+        Log.i("State of qsTile", isQsTileNull.toString())
+
+        if (qsTile == null) {
+            return
+        }
+
+        when(currentMode) {
+            AudioManager.RINGER_MODE_NORMAL -> {
+                audioManager.ringerMode = AudioManager.RINGER_MODE_VIBRATE
+                qsTile.label = "Vibrate"
+            }
+            AudioManager.RINGER_MODE_VIBRATE -> {
+                audioManager.ringerMode = AudioManager.RINGER_MODE_SILENT
+                qsTile.label = "Silent"
+            }
+            AudioManager.RINGER_MODE_SILENT -> {
+                audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
+                qsTile.label = "Normal"
+            }
+
+        }
+        qsTile.updateTile()
+    }
+
+}
