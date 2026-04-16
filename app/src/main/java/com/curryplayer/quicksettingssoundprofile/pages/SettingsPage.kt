345261aa7cd9195bdc5c5e05a1a6fa6b598a70d3
Tizian Everke <40740251+CurryPlayer@users.noreply.github.com>
2026-04-16 10:25:15 +0200

    Add verticalScroll to be able to scroll if not all information are shown on screen

--- app/src/main/java/com/curryplayer/quicksettingssoundprofile/pages/SettingsPage.kt
+++ app/src/main/java/com/curryplayer/quicksettingssoundprofile/pages/SettingsPage.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -38,6 +40,7 @@ fun RenderSettingsPage(
             Column(
                 modifier = Modifier
                     .padding(contentPadding)
+                    .verticalScroll(rememberScrollState())
             ) {
                 if (!hasPermission) {
                     RenderGrantPermissionCard(ctx)
