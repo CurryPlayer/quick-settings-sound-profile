7cd8ed33a0091819f47e0037293042bfa97bef33
Tizian Everke <40740251+CurryPlayer@users.noreply.github.com>
2026-05-11 11:15:56 +0200

    Update targetSdk to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.curryplayer.quicksettingssoundprofile"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 5
         versionName = "0.2.2"
 
