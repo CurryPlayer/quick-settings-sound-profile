d080ebe3c70eba8ade6e8b9e866562b2b71fd03b
Tizian Everke <40740251+CurryPlayer@users.noreply.github.com>
2026-06-20 21:15:29 +0200

    Reorder SDK version checks in ZenRuleUtils

--- app/src/main/java/com/curryplayer/quicksettingssoundprofile/utils/ZenRuleUtils.kt
+++ app/src/main/java/com/curryplayer/quicksettingssoundprofile/utils/ZenRuleUtils.kt
@@ -172,14 +172,14 @@ object ZenRuleUtils {
             .showInAmbientDisplay(true)
             .showInNotificationList(true)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            zenPolicyBuilder
-                .allowConversations(ZenPolicy.CONVERSATION_SENDERS_NONE)
-        }
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
             zenPolicyBuilder
                 .allowPriorityChannels(false)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            zenPolicyBuilder
+                .allowConversations(ZenPolicy.CONVERSATION_SENDERS_NONE)
         }
 
         return zenPolicyBuilder.build()
