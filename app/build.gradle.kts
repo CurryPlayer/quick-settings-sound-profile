3d0e6b657d34d76b044dc9084258ac0e1d38d447
Tizian Everke <40740251+CurryPlayer@users.noreply.github.com>
2026-07-19 22:54:24 +0200

    Update versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.curryplayer.quicksettingssoundprofile"
         minSdk = 24
         targetSdk = 37
-        versionCode = 8
-        versionName = "1.0.0"
+        versionCode = 9
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
