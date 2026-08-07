92e8d3c1d6da63734d71fdb68b33d74e09bbe04c
Tizian Everke <40740251+CurryPlayer@users.noreply.github.com>
2026-08-07 16:18:57 +0200

    Add tests for Icon Theme getIconForMode method

--- app/src/test/java/com/curryplayer/quicksettingssoundprofile/models/IconThemeTest.kt
+++ app/src/test/java/com/curryplayer/quicksettingssoundprofile/models/IconThemeTest.kt
@@ -1,5 +1,6 @@
 package com.curryplayer.quicksettingssoundprofile.models
 
+import android.media.AudioManager
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -24,5 +25,20 @@ class IconThemeTest {
         // Fallback should be VOLUME_DEFAULT
         assertEquals(IconTheme.VOLUME_DEFAULT, IconTheme.fromOrdinal(-1))
         assertEquals(IconTheme.VOLUME_DEFAULT, IconTheme.fromOrdinal(999))
+    }
+
+    @Test
+    fun testGetIconForMode() {
+        // VOLUME_DEFAULT
+        assertEquals(IconTheme.VOLUME_DEFAULT.ringIcon, IconTheme.VOLUME_DEFAULT.getIconForMode(AudioManager.RINGER_MODE_NORMAL))
+        assertEquals(IconTheme.VOLUME_DEFAULT.vibrateIcon, IconTheme.VOLUME_DEFAULT.getIconForMode(AudioManager.RINGER_MODE_VIBRATE))
+        assertEquals(IconTheme.VOLUME_DEFAULT.silentIcon, IconTheme.VOLUME_DEFAULT.getIconForMode(AudioManager.RINGER_MODE_SILENT))
+        assertEquals(IconTheme.VOLUME_DEFAULT.ringIcon, IconTheme.VOLUME_DEFAULT.getIconForMode(-1)) // Fallback
+
+        // NOTIFICATIONS
+        assertEquals(IconTheme.NOTIFICATIONS.ringIcon, IconTheme.NOTIFICATIONS.getIconForMode(AudioManager.RINGER_MODE_NORMAL))
+        assertEquals(IconTheme.NOTIFICATIONS.vibrateIcon, IconTheme.NOTIFICATIONS.getIconForMode(AudioManager.RINGER_MODE_VIBRATE))
+        assertEquals(IconTheme.NOTIFICATIONS.silentIcon, IconTheme.NOTIFICATIONS.getIconForMode(AudioManager.RINGER_MODE_SILENT))
+        assertEquals(IconTheme.NOTIFICATIONS.ringIcon, IconTheme.NOTIFICATIONS.getIconForMode(-1)) // Fallback
     }
 }
