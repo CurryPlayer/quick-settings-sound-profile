26466439b834bd1f1b18d1dc4dbe8f6556a94091
Tizian Everke <40740251+CurryPlayer@users.noreply.github.com>
2026-09-21 21:52:07 +0200

    Introduce new DataStoreManager.kt functions to receive data

--- app/src/main/java/com/curryplayer/quicksettingssoundprofile/data/DataStoreManager.kt
+++ app/src/main/java/com/curryplayer/quicksettingssoundprofile/data/DataStoreManager.kt
@@ -5,6 +5,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
@@ -16,6 +17,9 @@ class DataStoreManager(private val context: Context) {
     companion object {
         val ZEN_RULE_ID = stringPreferencesKey("zen_rule_id")
         val ICON_THEME = intPreferencesKey("icon_theme")
+        val TIMER_END_TIME = longPreferencesKey("timer_end_time")
+        val PREVIOUS_RINGER_MODE = intPreferencesKey("previous_ringer_mode")
+        val LAST_SELECTED_MUTE_DURATION_MINUTES = intPreferencesKey("last_selected_mute_duration_minutes")
     }
 
     val zenRuleId: Flow<String> = context.dataStore.data.map { preferences ->
@@ -24,6 +28,18 @@ class DataStoreManager(private val context: Context) {
 
     val iconTheme: Flow<Int> = context.dataStore.data.map { preferences ->
         preferences[ICON_THEME] ?: 0
+    }
+
+    val timerEndTime: Flow<Long> = context.dataStore.data.map { preferences ->
+        preferences[TIMER_END_TIME] ?: 0L
+    }
+
+    val previousRingerMode: Flow<Int> = context.dataStore.data.map { preferences ->
+        preferences[PREVIOUS_RINGER_MODE] ?: -1
+    }
+
+    val lastMuteDurationMinutes: Flow<Int> = context.dataStore.data.map { preferences ->
+        preferences[LAST_SELECTED_MUTE_DURATION_MINUTES] ?: 60
     }
 
     suspend fun setZenRuleId(value: String) {
@@ -35,6 +51,26 @@ class DataStoreManager(private val context: Context) {
     suspend fun setIconTheme(value: Int) {
         context.dataStore.edit { preferences ->
             preferences[ICON_THEME] = value
+        }
+    }
+
+    suspend fun saveMuteDurationMinutes(minutes: Int) {
+        context.dataStore.edit { preferences ->
+            preferences[LAST_SELECTED_MUTE_DURATION_MINUTES] = minutes
+        }
+    }
+
+    suspend fun saveTimer(endTime: Long, previousMode: Int) {
+        context.dataStore.edit { preferences ->
+            preferences[TIMER_END_TIME] = endTime
+            preferences[PREVIOUS_RINGER_MODE] = previousMode
+        }
+    }
+
+    suspend fun clearTimer() {
+        context.dataStore.edit { preferences ->
+            preferences[TIMER_END_TIME] = 0L
+            //preferences[PREVIOUS_RINGER_MODE] = -1
         }
     }
 }
